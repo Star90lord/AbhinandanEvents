e3c722b80d87afa9f266d09374deac4c9edf709a
Vikram Kumar Chaturvedi <vikramchaturvedi656@gmail.com>
2026-02-28 10:59:20 +0530

    Change 'Home' link text to 'Homes'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <nav className="bg-blue-600 text-white p-4 flex justify-between">
       <h1 className="font-bold">Abhinandan Events</h1>
       <div className="space-x-4">
-        <Link to="/">Home</Link>
+        <Link to="/">Homes</Link>
         <Link to="/login">Login</Link>
         <Link to="/signup">Signup</Link>
       </div>
